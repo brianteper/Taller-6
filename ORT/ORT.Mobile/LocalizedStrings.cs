b1b78766a1f91d34e55e1836b76cea0cf1ed1728
BrianTeper <brian.teper@outlook.com>
2014-05-01 23:08:04 -0300

    Features file and VS project

---
+++ ORT/ORT.Mobile/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ORT.Resources;
+
+namespace ORT
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
