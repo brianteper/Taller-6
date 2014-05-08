7c873a293f676aa5c1117884086a605afb1bfbe0
BrianTeper <brian.teper@outlook.com>
2014-05-08 19:52:12 -0300

    Form Validations

--- ORT/ORT.Mobile/MainPage.xaml.cs
+++ ORT/ORT.Mobile/MainPage.xaml.cs
@@ -10,6 +10,8 @@ using Microsoft.Phone.Shell;
 using Microsoft.Phone.Maps.Toolkit;
 using System.Device.Location;
 using Microsoft.Phone.Maps;
+using System.Windows.Media;
+using System.Text.RegularExpressions;
 
 namespace ORT.Mobile
 {
@@ -37,6 +39,90 @@ namespace ORT.Mobile
             {
                 App.ViewModel.LoadData();
             }
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            dynamic form = new 
+            {
+                Nombre = this.Nombre.Text,
+                Apellido = this.Apellido.Text,
+                Mail = this.Mail.Text,
+                UstedEs = this.UstedEs.SelectedItem.ToString(),
+                TieneInteres = this.TieneInteres.SelectedItem.ToString(),
+                Mensaje = this.Mensaje.Text
+            };
+
+            if (form.Nombre == String.Empty)
+            {
+                GeneralTransform transform = this.Nombre.TransformToVisual(this.ConsultasSV);
+                Point textBoxPosition = transform.Transform(new Point(0, 0));
+                this.ConsultasSV.ScrollToVerticalOffset(textBoxPosition.Y);
+                this.Nombre.Focus();
+
+                MessageBox.Show("Por favor, ingrese su nombre.");
+                return;
+            }
+
+            if (form.Apellido == String.Empty)
+            {
+                GeneralTransform transform = this.Apellido.TransformToVisual(this.ConsultasSV);
+                Point textBoxPosition = transform.Transform(new Point(0, 0));
+                this.ConsultasSV.ScrollToVerticalOffset(textBoxPosition.Y);
+                this.Apellido.Focus();
+
+                MessageBox.Show("Por favor, ingrese su apellido.");
+                return;
+            }
+
+            if (form.Mail == String.Empty)
+            {
+                GeneralTransform transform = this.Mail.TransformToVisual(this.ConsultasSV);
+                Point textBoxPosition = transform.Transform(new Point(0, 0));
+                this.ConsultasSV.ScrollToVerticalOffset(textBoxPosition.Y);
+                this.Mail.Focus();
+
+                MessageBox.Show("Por favor, ingrese su mail.");
+                return;
+            }
+
+            bool isEmail = Regex.IsMatch(form.Mail, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z");
+
+            if (!isEmail)
+            {
+                GeneralTransform transform = this.Mail.TransformToVisual(this.ConsultasSV);
+                Point textBoxPosition = transform.Transform(new Point(0, 0));
+                this.ConsultasSV.ScrollToVerticalOffset(textBoxPosition.Y);
+                this.Mail.Focus();
+
+                MessageBox.Show("Por favor, ingrese un mail válido.");
+                return;
+
+            }
+
+            if (form.Mensaje == String.Empty)
+            {
+                GeneralTransform transform = this.Mensaje.TransformToVisual(this.ConsultasSV);
+                Point textBoxPosition = transform.Transform(new Point(0, 0));
+                this.ConsultasSV.ScrollToVerticalOffset(textBoxPosition.Y);
+                this.Mensaje.Focus();
+
+                MessageBox.Show("Por favor, ingrese un mensaje.");
+                return;
+            }
+
+            //Send form to service
+
+            MessageBox.Show("El formulario ha sido enviado con éxito!");
+
+            this.Nombre.Text = String.Empty;
+            this.Apellido.Text = String.Empty;
+            this.Mail.Text = String.Empty;
+            this.UstedEs.SelectedIndex = 0;
+            this.TieneInteres.SelectedIndex = 0;
+            this.Mensaje.Text = String.Empty;
+
+            this.ConsultasSV.ScrollToVerticalOffset(0);
         }
     }
 }
